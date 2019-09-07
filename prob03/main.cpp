e096a742e724c02b06a7fb9d2d338933f8d511c4
Dustin_chavez <dustin_chavez@cs104u-6g.acad.fullerton.edu>
2019-09-07 14:27:01 -0700

    Calculated height of a person

--- prob03/main.cpp
+++ prob03/main.cpp
@@ -1,1 +1,16 @@
 // This program calculates a person's height in feet (') and inches (").
+
+#include <iostream>
+int main()
+{
+    int height;
+    int feet;
+    int inches;
+    std::cout << "How many feet tall? ";
+    std::cin >> feet;
+    std::cout << "How many inches tall?";
+    std::cin >> inches;
+    std::cout << "The person is "<< feet <<"' "<< inches <<"\" tall\n";
+
+    return 0;
+}
