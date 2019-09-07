f4b2afe328a54c58ea9f15e912a77915646f63d0
Dustin_chavez <dustin_chavez@cs104u-6g.acad.fullerton.edu>
2019-09-07 13:44:29 -0700

    did todo list

--- prob01/main.cpp
+++ prob01/main.cpp
@@ -1,14 +1,14 @@
 // This program tests output on the ASCII character set, and on the sizeof operator.
 
 #include <iostream>
-
+#include <string>
 int main()
 {
   // TODO#1: try changing these values to others found in the ASCII character set
   // The class Quick Reference Guide contains this chart on page 5
-  char my_char1 = 67;   // decimal
-  char my_char2 = 0x43; // hex
-  char my_char3 = 'C';  // character
+  char my_char1 = 65;   // decimal
+  char my_char2 = 0x41; // hex
+  char my_char3 = 'A';  // character
 
   std::cout << "Char1: " << my_char1 << std::endl;
   std::cout << "Char2: " << my_char2 << std::endl;
@@ -21,26 +21,32 @@ int main()
   // however, use a different method to do so for each cout statement
   // hint: use the ASCII character set
   std::cout << "+" << std::endl;
+  char my_char4 = 43;
+  char my_char5 = 0x2B;
+  std::cout << '+' <<std::endl;
+  std::cout << my_char4 << std::endl;
+  std::cout << my_char5 << std::endl;
 
   std::cout << std::endl; // blank line to separate the different exercises
 
   // TODO#3: update the following statement to include escape characters
   // note that clang does not require an escape character on the single quote
   // but many other compilers do require it
-  std::cout << "'Y'" << std::endl;
+  std::cout << "'/n'" << std::endl;
 
   std::cout << std::endl; // blank line to separate the different exercises
 
   // TODO#4: try changing the variable types and the data types in the
   // statements below to see what is returned by the sizeof operator
-  bool my_bool = 1;
-  std::cout << my_bool << ' ' << sizeof(my_bool) << '\n';
-  short my_int = 1;
-  std::cout << my_int << ' ' << sizeof(my_int) << '\n';
+  bool my_bool = 0;
+  std::string new_string;
+  std::cout << new_string << ' ' << sizeof(new_string) << '\n';
+  double my_float = 32;
+  std::cout << my_float << ' ' << sizeof(my_float) << '\n';
 
   double amount;
-  std::cout << "A float is stored in " << sizeof(float) << " bytes\n";
-  std::cout << "The variable \"amount\" is stored in " << sizeof(amount) << " bytes\n";
+  std::cout << "A string is stored in " << sizeof(std::string) << " bytes\n";
+  std::cout << "The variable \"amount\" is stored in " << sizeof(new_string) << " bytes\n";
 
   return 0;
 }
